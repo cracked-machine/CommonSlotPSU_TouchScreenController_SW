ca58ad5622bfaeadba0e7c7fbf9cbd2fc415efae
ChrisSutton <chrisjsutton@gmail.com>
2020-07-13 23:15:55 +0100

    Test

---
+++ HPECommonSlot_BreakoutModule_SW_F072/HPECommonSlot_BreakoutModule_SW_F072/Debug/Optimized_STM32_ILI9341/STMBasicGui.d
@@ -0,0 +1,14 @@
+Optimized_STM32_ILI9341/STMBasicGui.o: \
+ /home/chris/Projects/Embedded/Optimized_STM32_ILI9341/STMBasicGui.c \
+ /home/chris/Projects/Embedded/Optimized_STM32_ILI9341/STMBasicGui.h \
+ /home/chris/Projects/Embedded/Optimized_STM32_ILI9341/ILI9341_STM32_Driver.h \
+ /home/chris/Projects/Embedded/Optimized_STM32_ILI9341/fade_log_seq.h \
+ /home/chris/Projects/Embedded/Optimized_STM32_ILI9341/ILI9341_GFX.h
+
+/home/chris/Projects/Embedded/Optimized_STM32_ILI9341/STMBasicGui.h:
+
+/home/chris/Projects/Embedded/Optimized_STM32_ILI9341/ILI9341_STM32_Driver.h:
+
+/home/chris/Projects/Embedded/Optimized_STM32_ILI9341/fade_log_seq.h:
+
+/home/chris/Projects/Embedded/Optimized_STM32_ILI9341/ILI9341_GFX.h:
